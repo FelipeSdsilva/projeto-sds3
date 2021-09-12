183cea647e8ffc217867e0390f94cfecd1b82505
Felipe Sousa <felipe.fps09@hotmail.com>
2021-09-12 20:27:09 -0300

    First request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,9 +1,24 @@
+import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { SaleSum } from 'types/sale';
+import { BASE_URL } from 'utils/requests';
+type ChartData ={
+    labels: String[];
+    series: number[];
+}
 const DonutChart = () => {
-    const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
+    
+    let chartData : ChartData = { labels: [], series: []}
+
+    axios.get(BASE_URL+'/sale/amount-by-seller')
+        .then(response =>{
+            const data = response.data as SaleSum[];
+            const myLabels = data.map(x => x.sallerName);
+            const mySeries = data.map(x => x.sum);
+
+            chartData = { labels: myLabels, series: mySeries}
+            console.log(chartData);
+        })
     
     const options = {
         legend: {
@@ -13,8 +28,8 @@ const DonutChart = () => {
     
     return (
         <Chart 
-            options={{...options,labels: mockData.labels}}
-            series={mockData.series}
+            options={{...options, }}
+            series={chartData.series}
             type="donut"
             height="240"
         />
---
+++ frontend/src/types/sale.ts
@@ -0,0 +1,4 @@
+export type SaleSum = {
+    sallerName: string ,
+    sum: number
+}
