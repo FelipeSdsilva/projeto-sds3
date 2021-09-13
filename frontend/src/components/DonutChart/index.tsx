51050350c5db2aa5aa41d64b75eeeabe2a06ac78
Felipe Sousa <felipe.fps09@hotmail.com>
2021-09-13 02:42:39 -0300

    Enc var

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -23,7 +23,7 @@ const DonutChart = () => {
             setChartData ({ labels: myLabels, series: mySeries});
             console.log(chartData);
         });
-    },[]);    
+    });    
     
     const options = {
         legend: {
