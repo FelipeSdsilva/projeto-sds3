ed8d3bebcc619920b0319e180485ecb94de71fd1
Felipe Sousa <felipe.fps09@hotmail.com>
2021-09-13 02:30:16 -0300

    Enc var

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -65,22 +65,10 @@ const BarChart = () => {
             }
         },
     };
-    
-    const mockData = {
-        labels: {
-            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-        },
-        series: [
-            {
-                name: "% Sucesso",
-                data: [43.6, 67.1, 67.7, 45.6, 71.1]                   
-            }
-        ]
-    };
     return (
         <Chart 
             options={{...options,xaxis: chartData.labels}}
-            series={chartData   .series}
+            series={chartData.series}
             type="bar"
             height="240"
         />
--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -14,7 +14,7 @@ const DonutChart = () => {
     const [chartData, setChartData] = useState<ChartData>({ labels: [], series: []});
     
     useEffect(() =>{
-        axios.get(BASE_URL +'/sales/amount-by-seller')
+        axios.get(`${BASE_URL} +/sales/amount-by-seller`)
         .then(response =>{
             const data = response.data as SaleSum[];
             const myLabels = data.map(x => x.sallerName);
